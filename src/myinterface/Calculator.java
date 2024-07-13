9970833d99ed42348559421501c95b9223672e21
join0life <158464763+join0life@users.noreply.github.com>
2024-07-13 22:10:09 +0900

    Delete src directory

--- src/myinterface/Calculator.java
+++
@@ -1,9 +0,0 @@
-package myinterface;
-
-public interface Calculator {
-	public int add(int a, int b);
-	public int substract(int a, int b);
-	public int multiply(int a, int b);
-	public double divide(int a, int b);
-	public int modulas(int a, int b);
-}
--- src/myinterface/InterfaceTest_01.java
+++
@@ -1,9 +0,0 @@
-package myinterface;
-
-public interface InterfaceTest_01 {
-	public int add(int a, int b);
-	public int substract(int a, int b);
-	public int multiply(int a, int b);
-	
-	public static final int VALUE = 15; 
-}
